98a36a468b7c14d3078580249df148eee16d6b6e
Haefner <aaronhaefner@uchicago.edu>
2024-08-17 17:44:31 -0500

    make code tidy

--- code/main.py
+++ code/main.py
@@ -5,15 +5,31 @@ import logging
 import json
 from utils.utils import (
     set_device, load_tokenizer_model, make_model_contiguous,
-    load_train_test, preprocess_function, set_training_args,
-    set_trainer)
+    load_train_test, preprocess_function, process_tokenizer, 
+    set_training_args, set_trainer)
 from utils.globals import (DATASET, MODEL_NAME, HUB_MODEL_ID, HFTOKEN)
-from transformers import T5Tokenizer, T5ForConditionalGeneration, Trainer, TrainingArguments
+from transformers import (T5Tokenizer,
+                          T5ForConditionalGeneration,
+                          TrainingArguments,
+                          Trainer)
 from datasets import Dataset
+from dotenv import load_dotenv
 
-def first_stage_training():
+load_dotenv()
+
+def first_stage_training(model_name: str, push_to_hub: bool=False) -> None:
+    """
+    Load the model and tokenizer from the 
+    Hugging Face hub and train it on the input dataset.
+
+    Args:
+        model_name (str): The model name to load.
+        push_to_hub (bool): Whether to push the trained model to Hugging Face.
+    
+    Returns: None
+    """
     device = set_device()
-    tokenizer, model = load_tokenizer_model(device)
+    tokenizer, model = load_tokenizer_model(model_name, device)
 
     logging.info(f"Model loaded on {device}")
     make_model_contiguous(model)
@@ -21,17 +37,10 @@ def first_stage_training():
     # Load the dataset
     train_dataset, test_dataset = load_train_test(DATASET)
 
-    tokenized_train_dataset = train_dataset.map(
-        lambda examples: preprocess_function(examples, tokenizer),
-        batched=True,
-        remove_columns=train_dataset.column_names
-    )
-
-    tokenized_test_dataset = test_dataset.map(
-        lambda examples: preprocess_function(examples, tokenizer),
-        batched=True,
-        remove_columns=test_dataset.column_names
-    )
+    tokenized_train_dataset, tokenized_test_dataset = process_tokenizer(
+        tokenizer,
+        train_dataset,
+        test_dataset)
 
     logging.info("General dataset loaded and tokenized")
 
@@ -48,6 +57,8 @@ def first_stage_training():
     )
 
     trainer.train()
+    if push_to_hub:
+        trainer.push_to_hub()
     
     # Save the model and tokenizer
     model.save_pretrained("general_model")
@@ -56,18 +67,28 @@ def first_stage_training():
     logging.info("First stage training completed and model saved")
 
 
-def second_stage_training():
+def second_stage_training(examples_file: str, push_to_hub: bool=False) -> None:
+    """
+    Load the previously trained model and fine-tune it on domain-specific data.
+
+    Args:
+        examples_file (str): The path to the domain-specific data with
+        examples including questions and answers (or queries).
+
+    Returns: None
+    """
     device = set_device()
 
     # Load the previously trained model
     tokenizer = T5Tokenizer.from_pretrained("general_model")
-    model = T5ForConditionalGeneration.from_pretrained("general_model").to(device)
+    model = T5ForConditionalGeneration.from_pretrained(
+        "general_model").to(device)
 
     logging.info(f"Model loaded for fine-tuning on {device}")
     make_model_contiguous(model)
 
     # Load healthcare-specific dataset
-    with open('../input/combined_questions.json', 'r') as f:
+    with open(examples_file, 'r') as f:
         healthcare_data = json.load(f)
 
     train_dataset = Dataset.from_list(healthcare_data)
@@ -90,7 +111,7 @@ def second_stage_training():
         per_device_train_batch_size=4,
         num_train_epochs=1,
         weight_decay=0.01,
-        push_to_hub=False,
+        push_to_hub=push_to_hub,
         hub_token=HFTOKEN,
         remove_unused_columns=False,
     )
@@ -110,20 +131,25 @@ def second_stage_training():
     model.save_pretrained("healthcare_model")
     tokenizer.save_pretrained("healthcare_model")
 
-    logging.info("Second stage training (fine-tuning) completed and model saved")
+    logging.info("Fine-tuning stage completed and model saved")
 
 
 if __name__ == '__main__':
-    # Capture arg from command line to set train/eval/sql_eval mode(s)
+    push_to_hub = False
+    model_name = MODEL_NAME
+
     if len(sys.argv) < 2:
-        raise ValueError("Please provide a mode: first_stage_training or second_stage_training")
+        raise ValueError("Please provide a mode: \
+                         first_stage_training or second_stage_training")
     mode = sys.argv[1]
     if mode == "first_stage_training":
-        first_stage_training()
+        first_stage_training(model_name, push_to_hub)
     elif mode == "second_stage_training":
+        examples_file = "'../input/question_query.json"
         second_stage_training()
     elif mode == "full":
         first_stage_training()
         second_stage_training()
     else:
-        raise ValueError("Invalid mode. Please provide either first_stage_training or second_stage_training")
+        raise ValueError("Invalid mode. Please provide either \
+                         first_stage_training or second_stage_training")
