44d7f690e660e7a2cbd26fe3ba8aff333d80387f
Haefner <aaronhaefner@uchicago.edu>
2024-08-17 23:39:30 -0500

    fine tuning sql question paraphrasing

--- code/app.py
+++ code/app.py
@@ -3,13 +3,16 @@ import os
 import sys
 import logging
 import json
+from transformers import (AutoTokenizer, AutoModelForSeq2SeqLM,
+                          Trainer, TrainingArguments)
+from datasets import Dataset
 from utils.globals import HFTOKEN
-from utils.utils import (set_device, load_tokenizer_model)
-from utils.generative_utils import (generate_sql_query,
-                                    generate_questions,
+from utils.utils import (set_device, process_tokenizer,
+                         load_tokenizer_model, load_train_test)
+from utils.generative_utils import (generate_sql_query, generate_questions,
                                     generate_multiple_questions,
                                     evaluate_generated_sql)
-from main import fine_tune_training
+from main import first_stage_training, fine_tune_training
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -17,30 +20,27 @@ HEALTH_DATASET = "Nicolybgs/healthcare_data"
 EXAMPLES = "../input/question_query.json"
 MODEL_ID = "PASS"
 
-if __name__ == '__main__':
+def paraphrase_sql_questions():
     device = set_device()
-    # model_name = "general_model"  # google/flan-t5-base
     model_name = "google/flan-t5-base"
-    tokenizer, model = load_tokenizer_model(model_name, device)
-    # examples = "../input/question_query.json"
-    examples = "../input/paraphrased_examples.json"
-    fine_tune_training(tokenizer, model, examples, device,
-                       save_name="domain_model2",
-                       save_model=True, push_to_hub=False)
-    
-    # Load the fine-tuned model
-    model_name = "domain_model2"  # Fine-tuned model name
-    tokenizer, model = load_tokenizer_model(model_name, device)
+    model_type = "T5"
 
-    # Input the question and expected SQL query
-    input_question = "Which providers have NPIs in the state of California>"
-    expected_sql = "SELECT npi FROM nppes WHERE plocstatename = 'CA';"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
+    dataset_name = "philikai/200k-Text2SQL"
 
-    # Generate the corresponding SQL query for the input question
-    generated_sql_query = generate_sql_query(input_question, model, tokenizer, device)
-    logging.info(f"Generated SQL Query: {generated_sql_query}")
-    # INFO:root:Generated SQL Query: NPIs_state_name = 'California'
+    # Load and process the datasets
+    train_dataset, test_dataset = load_train_test(dataset_name)
+    tokenized_train_dataset, tokenized_test_dataset = process_tokenizer(
+        tokenizer, 
+        train_dataset, 
+        test_dataset, 
+        model_type=model_type
+    )
 
-    # Evaluate the generated SQL query against the expected result
-    # evaluation_result = evaluate_generated_sql(generated_sql_query, expected_sql)
-    # logging.info(f"Evaluation Result: Match: {evaluation_result['match']}, BLEU Score: {evaluation_result['bleu_score']}")
+    # Train the model using the tokenized datasets
+    first_stage_training(tokenizer, model, device, 
+                         tokenized_train_dataset, tokenized_test_dataset)
+
+if __name__ == '__main__':
+    paraphrase_sql_questions()
